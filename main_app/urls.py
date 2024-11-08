ad62548476866b31b76a35c88790fcabde84a382
Sammy <samanthabeltran1993@gmail.com>
2024-11-07 19:52:26 -0600

    work in progress

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     #routes will be added here
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cat-index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Import HttpResponse to send text-based responses
-from django.http import HttpResponse
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home/home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+
+def cat_index(request):
+    cats = Cat.objects.all()
+    # Render the cats/index.html template with the cats data
+    return render(request, 'cats/index.html', {'cats': cats})
